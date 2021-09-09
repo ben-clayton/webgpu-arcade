01bcc450a90bbdd64f180ef0a38c9d78a24dac81
Brandon Jones <tojiro@gmail.com>
2021-09-08 20:36:23 -0700

    Merging bumpy into this repo for the moment, since it'll be easier to iterate on that way

---
+++ bumpy/js/main.js
@@ -0,0 +1,77 @@
+import { Transform } from 'toro/core/transform.js';
+import { Camera } from 'toro/core/camera.js';
+import { PointLight, AmbientLight, DirectionalLight } from 'toro/core/light.js';
+import { Skybox } from 'toro/core/skybox.js';
+import { Mesh, AABB } from 'toro/core/mesh.js';
+
+import { GltfLoader } from 'toro/loaders/gltf.js';
+
+import { WebGPUWorld } from 'toro/webgpu/webgpu-world.js';
+import { WebGPULightSpriteSystem } from 'toro/webgpu/webgpu-light-sprite.js';
+
+import { vec3, quat } from 'gl-matrix';
+
+import Stats from 'stats.js';
+
+const stats = new Stats();
+document.body.appendChild(stats.dom);
+
+const canvas = document.querySelector('canvas');
+
+const world = new WebGPUWorld(canvas);
+
+world.registerRenderSystem(WebGPULightSpriteSystem);
+
+const renderer = await world.renderer();
+
+const gltfLoader = new GltfLoader(renderer);
+
+const projection = new Camera();
+projection.zNear = 1;
+projection.zFar = 1024;
+
+const cameraOrientation = quat.create();
+quat.rotateX(cameraOrientation, cameraOrientation, -Math.PI * 0.5);
+
+const camera = world.create(
+  new Transform({ position: [0, 65, 0], orientation: cameraOrientation }),
+  projection
+);
+
+// Add some lights
+world.create(
+  // A nice bright sunlight
+  new DirectionalLight({
+    direction: [0.2, 0.7, -0.5],
+    color: [1.0, 1.0, 0.7],
+    intensity: 5
+  }),
+  new AmbientLight(0.05, 0.05, 0.05)
+);
+
+// Load the ship models
+const shipMeshes = {};
+gltfLoader.fromUrl('./media/models/ships.glb').then(scene => {
+  for (const mesh of scene.meshes) {
+    shipMeshes[mesh.name] = mesh;
+  }
+
+  // Add the player
+  const player = world.create(shipMeshes.Player, new Transform({ position: [0, 0, 50] }));
+
+  world.create(shipMeshes.Heavy, new Transform({ position: [-20, 0, -50] }));
+  world.create(shipMeshes.Light, new Transform({ position: [-12, 0, -50] }));
+  world.create(shipMeshes.Laser, new Transform({ position: [-4, 0, -50] }));
+  world.create(shipMeshes.MultiGun, new Transform({ position: [4, 0, -50] }));
+  world.create(shipMeshes.Missile, new Transform({ position: [12, 0, -50] }));
+  world.create(shipMeshes.Mine, new Transform({ position: [20, 0, -50] }));
+});
+
+function onFrame() {
+  requestAnimationFrame(onFrame);
+
+  stats.begin();
+  world.execute();
+  stats.end();
+}
+requestAnimationFrame(onFrame);
